4ca7775858ef2513de113dc191f81a04c6986601
Derine Banis <d.banis.13@gmail.com>
2022-06-13 13:47:44 -0700

    Added hello page. Express sends the html body noted.

--- express-server.js
+++ express-server.js
@@ -6,13 +6,19 @@ const urlDataBase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.ca'
 };
-
+// GET /
 app.get('/', (req, res) => {
   res.send('Hello friend!');
 });
 
+// GET /urls.json
 app.get('/urls.json', (req, res) => {
   res.json(urlDataBase)
+})
+
+// GET /hello
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n')
 })
 
 app.listen(PORT, () => {

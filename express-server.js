7cdbafdd79d4d3ed4006e435f32ff461f9170ed5
Derine Banis <d.banis.13@gmail.com>
2022-06-15 11:27:08 -0700

    Added get route for /register

--- express-server.js
+++ express-server.js
@@ -77,6 +77,12 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
+// GET /register
+app.get('/register', (req,res)=> {
+  const templateVars = {username:req.cookies['username']}
+  res.render('register', templateVars)
+})
+
 /////////////////
 // POST ROUTES //
 /////////////////
